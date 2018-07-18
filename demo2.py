89652298f98a961761bb6a21a5419c393dd11667
bury <dzsbury@gmail.com>
2018-07-18 11:41:33 +0800

    the last modified demo2.py

--- demo2.py
+++ demo2.py
@@ -1,12 +1,13 @@
-def print_lol(the_list,level=0):
+﻿def print_lol(the_list,indent=False,level=0):
 	"""第一个参数为位置参数，可以为任意python列表
 	    第二个参数为遇到嵌套列表时候插入制表符名为"level"。"""
 	for each_item in the_list:
 		if isinstance(each_item,list):
-			print_lol(each_item,level+1)
+			print_lol(each_item,indent,level+1)
 		else:
-			for tab_stop in range(level):
-				print("\t",end='')
+			if indent:	
+				for tab_stop in range(level):
+					print("\t",end='')
 			print(each_item)
 
 
